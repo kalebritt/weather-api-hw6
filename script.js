e91c21e0eaff3012dae18be7f75328aadf208bf5
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-07-28 09:02:50 -0600

    added title to html, created weatherApi variable and added units=imperial to https

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+//console log "hello" to ensure js is connected to html
 console.log("hello");
+
+//create variables
+var weatherApi =
+  "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&units=imperial&appid={API key}";
