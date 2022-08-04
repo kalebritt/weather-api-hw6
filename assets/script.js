18d170fecd92b4b6a07bb0eaab4ce1aea3f40709
kalebritt <68469962+kalebritt@users.noreply.github.com>
2022-08-04 11:47:49 -0600

    lots of code!!!! created new index and added js to dynamically manipulate html

--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,8 @@ var weatherApi =
 
 var apiKey = "8d711c11700284f25c465cc9cf75be69";
 var form = document.getElementById("search-bar");
+var currentWeatherEl = document.querySelector(".current-weather");
+var forecastWeatherEl = document.querySelector(".forecast");
 
 //action when 'submitted'
 form.addEventListener("submit", function (event) {
@@ -24,32 +26,74 @@ function getCoordinates(cityName) {
       console.log(data);
       console.log(data[0].lat);
       console.log(data[0].lon);
-      fetchCurrentWeather(data[0].lat, data[0].lon);
+      fetchCurrentWeather(data[0].lat, data[0].lon, data[0].name);
       fetchForecast(data[0].lat, data[0].lon);
     });
 }
-function fetchCurrentWeather(lat, lon) {
+function fetchCurrentWeather(lat, lon, name) {
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
-  )
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-}
-
-function fetchForecast(lat, lon) {
-  fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
   )
     .then((response) => response.json())
     .then((data) => {
-      //for loop to change where to begin in array and where to jump in array
-      for (let i = 6; i < data.list.length; i += 8) {
-        console.log(data.list[i]);
-        //   var el = getElementById("#city-list");
-        //   el.text = data.list[i];
-        //   el.value = data.list[i];
-
-        //   select.add(el);
-      }
+      renderCurrentWeather(data, name);
+      renderForecast(data);
     });
+}
+
+// function fetchForecast(lat, lon) {
+//   fetch(
+//     `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`
+//   )
+//     .then((response) => response.json())
+//     .then((data) => {
+//       //for loop to change where to begin in array and where to jump in array
+//       for (let i = 6; i < data.list.length; i += 8) {
+//         console.log(data.list[i]);
+//         //   var el = getElementById("#city-list");
+//         //   el.text = data.list[i];
+//         //   el.value = data.list[i];
+
+//         //   select.add(el);
+//       }
+//     });
+// }
+
+function renderCurrentWeather(data, name) {
+  console.log(data);
+  currentWeatherEl.innerHTML = `
+  <div>
+    <h2>${name}</h2>
+    <img src="http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png"/>
+  </div>
+  <div>
+    <p>Humidity</p>
+    <p>Temperature</p>
+    <p>Wind</p>
+  </div>
+  
+  `;
+}
+
+function renderForecast(data) {
+  forecastWeatherEl.innerHTML = "";
+  for (let index = 0; index < 5; index++) {
+    const element = data.daily[index];
+    console.log(element);
+    var card = document.createElement("div");
+    card.classList.add("card");
+    card.innerHTML = `
+    <div class="card-header">
+      <h5>${new Date(element.dt * 1000).toLocaleString("en-US", {
+        weekday: "long",
+      })}</h5>
+    </div>
+    <div class="card-body">
+        <img src="" alt="">
+        <p class="card-text">Temperature: ${element.temp.day}</p>
+        <p class="card-text"></p>
+    </div>
+    `;
+    forecastWeatherEl.append(card);
+  }
 }
